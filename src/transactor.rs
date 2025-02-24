86c0ed3c4121f6d8f47d62d28e4f5e5332d2b38a
fixpt <57822954+fixpt@users.noreply.github.com>
2025-02-24 07:39:13 -0500

    More work towards transactor

--- src/transactor.rs
+++ src/transactor.rs
@@ -1,28 +1,50 @@
 use crate::{
     message::Message,
     router::{Actor, Address, Context},
+    value::Value,
 };
 
-pub struct Transactor {
+pub struct Transactor<T> {
     address: Address,
-    state: State,
+    transaction: T,
 }
 
-enum State {
-    Start,
+pub trait Transaction: Sized + Send {
+    fn request(&self) -> Request<Self>;
 }
 
-impl Transactor {
-    pub fn new(address: Address) -> Transactor {
+pub enum Request<T> {
+    Read {
+        address: Address,
+        fulfill: fn(&mut T, Value),
+    },
+    Write {
+        address: Address,
+        value: Value,
+        fulfill: fn(&mut T),
+    },
+}
+
+impl<T: Default> Transactor<T> {
+    pub fn new(address: Address) -> Transactor<T> {
         Transactor {
             address,
-            state: State::Start,
+            transaction: Default::default(),
         }
     }
 }
 
-impl Actor for Transactor {
-    fn init(&mut self, ctx: Context) {}
+impl<T: Transaction> Actor for Transactor<T> {
+    fn init(&mut self, ctx: Context) {
+        match self.transaction.request() {
+            Request::Read { address, fulfill } => todo!(),
+            Request::Write {
+                address,
+                value,
+                fulfill,
+            } => todo!(),
+        }
+    }
 
     fn handle(&mut self, sender: Address, message: Message, ctx: Context) {}
 }
